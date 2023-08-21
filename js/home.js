0fae1b65ad3de00b10e72b403cbd277a9465ce1e
Hung0326 <trunghung5055@gmail.com>
2023-08-21 09:58:53 +0700

    update bg iframe

--- js/home.js
+++ js/home.js
@@ -36,7 +36,7 @@
       loop: true,
       items: 1
   });
-  $('.v-iframe').contents().find('body').css({'background-color': '#f3f6ff'});
+  $('#v-iframe').contents().find('body').css({'background-color': '#f3f6ff'});
 
 })(jQuery);
 
